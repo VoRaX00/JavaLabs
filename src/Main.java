297a8ad8946ff0dd48e096dbbad97ba3ae92977d
nikita <nikitakerzhik@gmail.com>
2024-10-12 12:21:13 +1000

    realization lab2

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,6 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Checker bChecker = new SubstringChecker("beets");
-        Checker cChecker = new SubstringChecker("carrots");
-        Checker bothChecker = new AndChecker(bChecker, cChecker);
 
-        Checker aChecker = new SubstringChecker("artichokes");
-        Checker veggies = new AndChecker(bothChecker, aChecker);
-
-        System.out.println(bothChecker.accept("I love beets and carrots"));
-        System.out.println(bothChecker.accept("beets and great"));
-        System.out.println(veggies.accept("artichokes, beets, and carrots"));
     }
 }
---
+++ src/SkyView.java
@@ -0,0 +1,30 @@
+public class SkyView {
+    private final double[][] view;
+
+    public SkyView(int numRows, int numCols, double[] scanned) {
+        view = new double[numRows][numCols];
+        int idxScanned = 0;
+        boolean isLeft = true;
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < numCols; j++) {
+                if (isLeft) {
+                    view[i][j] = scanned[idxScanned];
+                } else {
+                    view[i][numCols - j - 1] = scanned[idxScanned];
+                }
+            }
+            isLeft = !isLeft;
+        }
+    }
+
+    public double getAverage(int startRow, int endRow, int startCol, int endCol) {
+        double result = 0;
+        for(int i=startRow; i<=endRow; i++) {
+            for(int j=startCol; j<=endCol; j++) {
+                result += view[i][j];
+            }
+        }
+        int cnt = (endRow - startRow + 1) * (endCol - startCol + 1);
+        return result / cnt;
+    }
+}
