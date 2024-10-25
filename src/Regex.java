c1a32e3b485bd319657fd50befb6dc992c339189
nikita <nikitakerzhik@gmail.com>
2024-10-25 14:10:39 +1000

    init lab4

---
+++ src/Regex.java
@@ -0,0 +1,8 @@
+import java.util.regex.Pattern;
+
+public class Regex {
+    public static boolean isCorrect(String str) {
+        Pattern pattern = Pattern.compile(".");
+        return false;
+    }
+}
