29356de23154a319c4ae802165313c9df2fb51fd
nikita <nikitakerzhik@gmail.com>
2024-10-25 16:02:07 +1000

    realization lab6

---
+++ src/People.java
@@ -0,0 +1,21 @@
+import java.io.Serializable;
+import java.util.Date;
+
+public class People implements Serializable {
+    private final String _name;
+    private final String _surname;
+    private final Date _birthYear;
+    private final String _address;
+
+    public People(String name, String surname, Date birthYear, String address) {
+        _name = name;
+        _surname = surname;
+        _birthYear = birthYear;
+        _address = address;
+    }
+
+    @Override
+    public String toString() {
+        return _name + " " + _surname + " " + _birthYear + " " + _address;
+    }
+}
--- src/StorageExp.java
+++
@@ -1,5 +0,0 @@
-public class StorageExp extends RuntimeException {
-    public StorageExp(String message) {
-        super(message);
-    }
-}
