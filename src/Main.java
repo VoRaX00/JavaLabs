a5e90313824eb742d4ae5836dde6d04311fc07c6
nikita <nikitakerzhik@gmail.com>
2024-10-26 12:55:54 +1000

    edit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
-import java.text.SimpleDateFormat;
-
+import java.io.IOException;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        People people = new People("Nikita", "Kerzhakov", 2004, "");
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        People people = new People("Nikita", "Kerzhakov", 2004, "Пермь");
+        PeopleToFile.OutputFile(people, "../test.txt");
+        System.out.println(PeopleToFile.ReadFile("../test.txt"));
     }
 }
