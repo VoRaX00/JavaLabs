de72aaf4a0cbc5616937afd969ca42ac4ba61923
nikita <nikitakerzhik@gmail.com>
2024-10-26 12:44:40 +1000

    edit

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,9 @@
 public class Main {
     public static void main(String[] args) {
         int [][] arr = {
-                {1, 2},
-                {1, 2},
+                {1, 2, 3},
+                {2, 3, 1},
+                {3, 1, 2},
         };
 
         System.out.println(ArrayTester.isLatin(arr));
