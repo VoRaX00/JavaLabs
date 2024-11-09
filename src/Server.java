f135b27d9b66baa3a0f5393601179f7f82e2a807
nikita <nikitakerzhik@gmail.com>
2024-11-09 21:29:08 +1000

    added server

---
+++ src/Server.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Server {
+    private final ServerSocket serverSocket;
+    private final List<Socket> clients;
+    private int clientNumber;
+
+    public Server(int port) throws IOException {
+        serverSocket = new ServerSocket(port);
+        clientNumber = 0;
+        clients = new ArrayList<>();
+    }
+
+    public void start() throws IOException {
+        try {
+            while (true) {
+                Socket socket = serverSocket.accept();
+                clients.add(socket);
+                new ServiceThread(clientNumber++, socket, clients).start();
+            }
+        } finally {
+            serverSocket.close();
+        }
+    }
+}
