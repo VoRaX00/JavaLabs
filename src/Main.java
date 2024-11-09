b7b46b6e567d2a7c5e431a0fc69befd5fe0ae612
nikita <nikitakerzhik@gmail.com>
2024-11-09 12:49:54 +1000

    init lab9

--- src/Main.java
+++ src/Main.java
@@ -6,31 +6,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        while (true) {
-            String[] params = getParams();
-            if (params == null) {
-                System.out.println("Параметры введены неверно");
-                break;
-            } else {
-                People people = new People(params[0], params[1], params[2], Integer.parseInt(params[3]));
-                List<People> peoples = PeopleRepo.addPeople(people);
-                System.out.println(peoples);
-            }
-        }
-    }
 
-    private static String[] getParams() {
-        System.out.println("Введите ваше ФИО и ваш возраст: (пример: Кержаков Никита Алексеевич 20)");
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        if (isCorrect(input)) {
-            return input.split(" ");
-        }
-        return null;
-    }
-
-    private static boolean isCorrect(String input) {
-        String regex = "^[А-ЯЁ][а-яё]+ [А-ЯЁ][а-яё]+ [А-ЯЁ][а-яё]+ (1[0-4][0-9]|[1-9]?[0-9]|150)$";
-        return input.matches(regex);
     }
 }
